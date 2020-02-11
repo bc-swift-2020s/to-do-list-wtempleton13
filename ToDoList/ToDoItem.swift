d5fe56e8b67abf9ae5257c092b2c1d5c6fb7f860
Will Templeton <templetw@bc.edu>
2020-02-10 21:51:40 -0500

    5.6 structs

---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by William Templeton on 2/10/20.
+//  Copyright © 2020 William Templeton. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+       var name: String
+       var date: Date
+       var notes: String
+   }
--- ToDoList/ToDoListViewController.swift
+++ ToDoList/ToDoListViewController.swift
@@ -10,22 +10,25 @@ import UIKit
 
 class ToDoListViewController: UIViewController {
     
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var addBarButton: UIBarButtonItem!
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
     
-    var toDoArray = ["learn","swift","to do","app","yes"]
+    var toDoItems: [ToDoItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        editBarButton.title = "Edit"
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowDetail" {
             let destination = segue.destination as! ToDoDetailTableViewController
             let selectedIndexPath = tableView.indexPathForSelectedRow!
-            destination.toDoItem = toDoArray[selectedIndexPath.row]
+            destination.toDoItem = toDoItems[selectedIndexPath.row]
         } else {
             if let selectedIndexPath = tableView.indexPathForSelectedRow {
                 tableView.deselectRow(at: selectedIndexPath, animated: true)
@@ -36,11 +39,11 @@ class ToDoListViewController: UIViewController {
     @IBAction func unwindFromDetail(segue: UIStoryboardSegue) {
         let source = segue.source as! ToDoDetailTableViewController
         if let selectedIndexPath = tableView.indexPathForSelectedRow {
-            toDoArray[selectedIndexPath.row] = source.toDoItem
+            toDoItems[selectedIndexPath.row] = source.toDoItem
             tableView.reloadRows(at: [selectedIndexPath], with: .automatic)
         } else {
-            let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
-            toDoArray.append(source.toDoItem)
+            let newIndexPath = IndexPath(row: toDoItems.count, section: 0)
+            toDoItems.append(source.toDoItem)
             tableView.insertRows(at: [newIndexPath], with: .bottom)
             tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
         }
@@ -62,26 +65,26 @@ class ToDoListViewController: UIViewController {
 extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("numberOfRowsInSection was just called. Returning \(toDoArray.count)")
-        return toDoArray.count
+        print("numberOfRowsInSection was just called. Returning \(toDoItems.count)")
+        return toDoItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("cellForRowAt was just called for indexPath.row = \(indexPath.row) which is the cell containing \(toDoArray[indexPath.row])")
+        print("cellForRowAt was just called for indexPath.row = \(indexPath.row) which is the cell containing \(toDoItems[indexPath.row])")
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = toDoArray[indexPath.row]
+        cell.textLabel?.text = toDoItems[indexPath.row].name
         return cell
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            toDoArray.remove(at: indexPath.row)
+            toDoItems.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        let itemToMove = toDoArray[sourceIndexPath.row]
-        toDoArray.remove(at: sourceIndexPath.row)
-        toDoArray.insert(itemToMove, at: destinationIndexPath.row)
+        let itemToMove = toDoItems[sourceIndexPath.row]
+        toDoItems.remove(at: sourceIndexPath.row)
+        toDoItems.insert(itemToMove, at: destinationIndexPath.row)
     }
 }
