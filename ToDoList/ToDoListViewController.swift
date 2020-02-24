7d4e2e79bf23554a7b16446e4b330214662a4c1e
Will Templeton <templetw@bc.edu>
2020-02-24 11:54:07 -0500

    5.10 notifications

--- ToDoList/ToDoListViewController.swift
+++ ToDoList/ToDoListViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ToDoListViewController: UIViewController {
     
@@ -23,12 +24,77 @@ class ToDoListViewController: UIViewController {
         tableView.dataSource = self
         editBarButton.title = "Edit"
         loadData()
+        authorizeLocalNotifactions()
     }
+    
+    func authorizeLocalNotifactions() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge,])  {(granted, error)
+            in
+            guard error == nil else {
+                print("ERROR \(error!.localizedDescription)")
+                return
+            }
+                if granted {
+                    print("Notifactions Authorization Granted")
+                } else {
+                    print("User has denied Notifcations")
+                }
+                
+            }
+
+        }
+    
+    func setNotifications() {
+        guard toDoItems.count > 0 else {
+            return
+        }
+        // remove all notifications
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        
+        // recreate with updated data
+        for index in 0..<toDoItems.count {
+            if toDoItems[index].reminderSet {
+                let toDoItem = toDoItems[index]
+                toDoItems[index].notificationID = setCalendarNotifications(title: toDoItem.name, subtitle: "", body: toDoItem.notes, badgeNumber: nil, sound: .default, date: toDoItem.date)
+
+            }
+        }
+    }
+    
+    func setCalendarNotifications(title: String, subtitle: String, body: String, badgeNumber: NSNumber?, sound: UNNotificationSound?, date: Date) -> String {
+        // create content:
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.subtitle = subtitle
+        content.body = body
+        content.sound = sound
+        content.badge = badgeNumber
+        
+        // create trigger
+        var dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+        dateComponents.second = 00
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        // create request
+        let notificationID = UUID().uuidString
+        let request = UNNotificationRequest(identifier: notificationID, content: content, trigger: trigger)
+        
+        // register request with notification center
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+            } else {
+                print("Notification scheduled \(notificationID), title: \(content.title)")
+            }
+        }
+        return notificationID
+    }
+
     
     func loadData() {
         let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         let documentURL = directoryURL.appendingPathComponent("todos")
-                   .appendingPathExtension("json")
+            .appendingPathExtension("json")
         
         guard let data = try? Data(contentsOf: documentURL) else {return}
         let jsonDecoder = JSONDecoder()
